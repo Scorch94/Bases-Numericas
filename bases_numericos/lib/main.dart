d584f59283b7ac90a3479504b1799c43beb61554
EricSena241 <ericsena241@gmail.com>
2024-04-12 16:53:38 +0100

    Eric

--- bases_numericos/lib/main.dart
+++ bases_numericos/lib/main.dart
@@ -1,115 +1,144 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculadora Numérica',
+      title: 'Conversor de Base Numérica',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Calculadora Numérica'),
+      home: ConverterPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ConverterPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ConverterPageState createState() => _ConverterPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ConverterPageState extends State<ConverterPage> {
+  String _selectedBaseInput = "Decimal";
+  String _selectedBaseOutput = "Decimal";
+  String _inputValue = "";
+  String _outputValue = "";
 
-  void _incrementCounter() {
+  void _convertValue(String value, String baseFrom, String baseTo) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      switch (baseFrom) {
+        case "Decimal":
+          _outputValue = _convertFromDecimal(int.parse(value), baseTo);
+          break;
+        case "Binário":
+          _outputValue = _convertFromBinary(value, baseTo);
+          break;
+        case "Octal":
+          _outputValue = _convertFromOctal(value, baseTo);
+          break;
+        case "Hexadecimal":
+          _outputValue = _convertFromHexadecimal(value, baseTo);
+          break;
+      }
     });
+  }
+
+  String _convertFromDecimal(int value, String baseTo) {
+    switch (baseTo) {
+      case "Decimal":
+        return value.toString();
+      case "Binário":
+        return value.toRadixString(2);
+      case "Octal":
+        return value.toRadixString(8);
+      case "Hexadecimal":
+        return value.toRadixString(16).toUpperCase();
+      default:
+        return "";
+    }
+  }
+
+  String _convertFromBinary(String value, String baseTo) {
+    int decimalValue = int.parse(value, radix: 2);
+    return _convertFromDecimal(decimalValue, baseTo);
+  }
+
+  String _convertFromOctal(String value, String baseTo) {
+    int decimalValue = int.parse(value, radix: 8);
+    return _convertFromDecimal(decimalValue, baseTo);
+  }
+
+  String _convertFromHexadecimal(String value, String baseTo) {
+    int decimalValue = int.parse(value, radix: 16);
+    return _convertFromDecimal(decimalValue, baseTo);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Conversor de Base Numérica'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Valor:'),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _inputValue = value;
+                  _convertValue(
+                      _inputValue, _selectedBaseInput, _selectedBaseOutput);
+                });
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20.0),
+            Text('Base de Entrada:'),
+            DropdownButton<String>(
+              value: _selectedBaseInput,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedBaseInput = newValue!;
+                  _outputValue = "";
+                });
+              },
+              items: <String>['Decimal', 'Binário', 'Octal', 'Hexadecimal']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
+            SizedBox(height: 20.0),
+            Text('Base de Saída:'),
+            DropdownButton<String>(
+              value: _selectedBaseOutput,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedBaseOutput = newValue!;
+                  _outputValue = "";
+                });
+              },
+              items: <String>['Decimal', 'Binário', 'Octal', 'Hexadecimal']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20.0),
+            Text('Resultado: $_outputValue'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
